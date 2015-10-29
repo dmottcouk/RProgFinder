8388e170c1af66a17732414b6686a9fdbac6a307
David Mottershead <david.mottershead1@ntlworld.com>
2015-10-29 10:57:59 +0000

    Initial commit

---
+++ app/src/main/java/uk/co/dmott/rprogfinder/HelpActivity.java
@@ -0,0 +1,35 @@
+package uk.co.dmott.rprogfinder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by david on 28/10/15.
+ */
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+}
